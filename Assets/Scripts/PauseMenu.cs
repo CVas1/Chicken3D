512ca0c57b11bb4c82dcc677395ac3ccfbc4d672
CVas1 <huseyinsoftware@gmail.com>
2023-07-13 01:17:12 +0300

    Bugs Fixed

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -6,10 +6,43 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     public bool isPaused = false;
+    [SerializeField] private GameObject UIPanel;
+    [SerializeField] private GameObject UICamera;
+    [SerializeField] private GameObject ChickenCounter;
+
+
+
+
+    void Start()
+    {
+        Observable.EveryUpdate()
+            .Where(_ => Input.GetKeyDown(KeyCode.Escape))
+            .Subscribe(_ => HandleEscapeKey())
+            .AddTo(this);
+    }
 
     public void Pause() { isPaused = true; }
     public void Resume() { isPaused = false; }
 
+    
 
+    void HandleEscapeKey()
+    {
+        if(isPaused)
+        {
+            Resume();
+            UICamera.SetActive(false);
+            UIPanel.SetActive(false);
+            ChickenCounter.SetActive(true);
+        }
+        else
+        {
+            Pause();
+            UICamera.SetActive(true); 
+            UIPanel.SetActive(true);
+            ChickenCounter.SetActive(false);
+
+        }
+    }
 
 }
